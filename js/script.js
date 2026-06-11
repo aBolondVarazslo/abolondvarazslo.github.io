c6936e00b210dbb7db4c16eaf50a0f54989678b9
Jack Woodland <woodlandjack9@gmail.com>
2026-06-11 13:19:04 +0100

    Made date in footer dynamixc

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@ const nav = document.getElementById('navbar');
 window.addEventListener('scroll', () => {
     nav.classList.toggle('scrolled', window.scrollY > 60);
 });
+
+document.getElementById("year").textContent = new Date().getFullYear();
